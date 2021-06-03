ea8e60462b14b0c90b0051ba880d5c3783e85af9
Luis Chavez <josephsiul15@gmail.com>
2021-06-02 19:48:54 -0500

    separating in components, add Link and History.push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+import ListBooks from './Components/ListBooks/ListBooks';
+import SearchBooks from './Components/SearchBooks/SearchBooks';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class BooksApp extends Component {
+  state = {
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    showSearchPage: false,
+  };
+
+  render() {
+    return (
+      <div className='app'>
+        <Route exact path='/' component={ListBooks} />
+        <Route exact path='/search' component={SearchBooks} />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default BooksApp;
